bea684b7d68fe5b6689dc7347b9e1148219badf3
Your Name <ogad874@gmail.com>
2026-06-21 17:50:59 -0400

    problem 1

--- M3/CommandLineCalculator.java
+++ M3/CommandLineCalculator.java
@@ -12,8 +12,8 @@ Challenge 1: Command-Line Calculator
 */
 
 public class CommandLineCalculator extends BaseClass {
-    private static String ucid = "mt85"; // <-- change to your ucid
-
+    private static String ucid = "omg6"; // <-- change to your ucid
+// omg6, 
     public static void main(String[] args) {
         printHeader(ucid, 1, "Objective: Implement a calculator using command-line arguments.");
 
@@ -26,15 +26,35 @@ public class CommandLineCalculator extends BaseClass {
         try {
             System.out.println("Calculating result...");
             // extract the equation (format is <num1> <operator> <num2>)
+            String number1String = args[0];
+            String operator = args[1];
+            String number2String = args[2];
 
             // check if operator is addition or subtraction
+            if (!operator.equals ("+") && !operator.equals ("-")){
+
+                System.out.println ("Only + and - are accepted");
+                return;
+            }
 
             // check the type of each number and choose appropriate parsing
+
+            double number1 = Double.parseDouble (number1String);
+            double number2 = Double.parseDouble (number2String);
+
 
             // generate the equation result (Important: ensure decimals display as the
             // longest decimal passed)
             // i.e., 0.1 + 0.2 would show as one decimal place (0.3), 0.11 + 0.2 would shows
             // as two (0.31), etc
+
+            int decimals1 = number1String.contains(".") ? number1String.length() - number1String.indexOf (".") -1:0;
+            int decimals2 = number2String.contains(".") ? number2String.length() - number2String.indexOf (".") -1:0;
+            int maxDecimal = Math.max(decimals1,decimals2);
+
+            Double result = operator.equals("+") ? number1+number2 : number1-number2;
+            String Result = String.format("%." + maxDecimal + "f", result);
+            System.out.println(number1String + " " + operator + " " + number2String + " = " + Result);
 
         } catch (Exception e) {
             System.out.println("Invalid input. Please ensure correct format and valid numbers.");
